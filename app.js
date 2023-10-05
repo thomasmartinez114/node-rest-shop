3966243e84c3131c6d8038d0e7ecf8a9d728db38
Thomas Martinez <thomas.martinez@igt.com>
2023-10-05 13:58:25 -0400

    CORS handling

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+// CORS handling
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // allow access to all with *
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-ALlow-Methods', 'PUT, POST, GET, PATCH, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
+
 // Routes which should handle requests
 const productRoutes = require('./api/routes/products');
 const productOrders = require('./api/routes/orders');
